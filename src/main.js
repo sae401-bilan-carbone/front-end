f492dd9bc5f610b34aa52ddea222d30d03205273
Ulysse Pennetier <upennetier@gmail.com>
2026-04-04 10:54:29 +0200

    config(): pinia, custom config file

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 import { createApp } from 'vue'
+import { i18n } from './i18n'
+import { useAuthStore } from './services/store/useAuthStore'
 import App from './App.vue'
 import router from './router'
-import { i18n } from './i18n'
 
 import './assets/styles/main.scss'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
+const pinia = createPinia()
+app.use(pinia)
+
+const authStore = useAuthStore()
+
 app.use(router)
 app.use(i18n)
-app.mount('#app')
+
+authStore.initAuth().then(() => {
+  app.mount('#app')
+})
